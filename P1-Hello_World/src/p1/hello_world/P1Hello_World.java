ceee5f995041a300c803c922b1e57279c84f0f79
Lucas Mancini <mancinilucas95@gmail.com>
2020-04-06 22:33:35 -0300

    Add files via upload

---
+++ P1-Hello_World/src/p1/hello_world/P1Hello_World.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1.hello_world;
+
+/**
+ *
+ * @author manci
+ */
+public class P1Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double number=32.2;
+      System.out.print(number);
+    }
+    
+}
